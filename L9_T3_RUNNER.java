e0e178fd9228c947dbcd6476ff091247990fbee0
ayeshakamran543 <80027904+ayeshakamran543@users.noreply.github.com>
2021-04-30 16:42:54 +0500

    Add files via upload

---
+++ L9_T3_RUNNER.java
@@ -0,0 +1,16 @@
+
+package student1;
+
+
+public class L9_T3_RUNNER {
+     public static void main(String[] args) {
+        PhdStudent s1=new PhdStudent();
+        GraduateStudent s2=new GraduateStudent();
+        s1.Take_exam();
+        s2.Take_exam();
+        
+        
+        
+    }
+    
+}
---
+++ LAB9_T1_RUNNER.java
@@ -0,0 +1,15 @@
+
+package clock;
+
+public class LAB9_T1_RUNNER {
+    public static void main(String[] args) {
+        ClockExtended c1=new ClockExtended(11,20,30);
+        ClockExtended c2=new ClockExtended(23,13,59);
+        c1.display();
+        c2.display();
+
+        
+        
+    }
+    
+}
